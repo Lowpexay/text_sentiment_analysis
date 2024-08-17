df9ead48e414d9c00fdba9c7241a9ffaa868a653
Gabriel Gramacho <ggramacho09@gmail.com>
2024-08-17 13:22:57 -0300

    :changing things

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from transformers import pipeline
 
 sentiment_pipeline = pipeline("sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
-
+ 
 def analyze_sentiment(text):
     result = sentiment_pipeline(text)[0]
     label = result['label']
